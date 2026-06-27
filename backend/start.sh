c675d73d54841bc0e781cc10b42fa1a86b011e37
media894 <media@oddinfotech.com>
2026-06-27 13:30:49 +0530

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -4,4 +4,4 @@ python manage.py collectstatic --noinput
 python manage.py fix_stuck_posts
 
 # Start Waitress in the foreground as a lightweight single-process WSGI server to prevent Out-Of-Memory (OOM) crashes
-waitress-serve --port=$PORT --threads=4 config.wsgi:application
+waitress-serve --port=$PORT --threads=2 config.wsgi:application
